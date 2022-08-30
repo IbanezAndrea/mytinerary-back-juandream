96e74404057f29f326d9c2c5a6654c79d2865a41
Ibañez Andrea <108688594+IbanezAndrea@users.noreply.github.com>
2022-08-30 10:38:00 -0300

    Update cities.js

--- routes/cities.js
+++ routes/cities.js
@@ -3,16 +3,16 @@ const router= express.Router();
 
 const {addCity,getOneCity,getCities,modifyCity,removeCity} = require ('../controllers/cityController');
 
-const cityController= require('../controllers/cityController');
-const addCity= cityController.addCity;
+//const cityController= require('../controllers/cityController');
+//const addCity= cityController.addCity;
 //create
-const getOneCity=cityController.getOneCity;
+//const getOneCity=cityController.getOneCity;
 //read
-const getCities=cityController.getCities;
+//const getCities=cityController.getCities;
 //read multiples
-const modifyCity=cityController.modifyCity;
+//const modifyCity=cityController.modifyCity;
 //update
-const removeCity=cityController.removeCity;
+//const removeCity=cityController.removeCity;
 //delete
 
 

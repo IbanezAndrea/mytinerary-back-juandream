31cbc9b101aa8894469bde793771891238183e0f
IbanezAndrea <mrs3lite@gmail.com>
2022-08-31 17:02:54 -0300

    updated getCitties

--- controllers/cityController.js
+++ controllers/cityController.js
@@ -1,3 +1,6 @@
+const { name } = require('ejs')
+const { query } = require('express')
+const { db } = require('../models/City')
 const City = require('../models/City')
 
 const cityController = {
@@ -5,14 +8,14 @@ const cityController = {
         try {
             let city = await new City(req.body).save()
             res.status("201").json({
-                message: "a new city has been added",
+                message: "A new city has been added",
                 response: city._id,
                 succes: true,
             })
         } catch (error) {
             console.log(error)
             res.status("400").json({
-                message: "could't add city",
+                message: "Your city could not be added",
                 succes: false,
             })
         }
@@ -23,44 +26,49 @@ const cityController = {
             let city = await City.findOne({ _id: id })
             if (city) {
                 res.status("200").json({
-                    message: "you get one city",
+                    message: "Found a city",
                     response: city,
                     succes: true,
                 })
             } else {
                 res.status("404").json({
-                    message: "could't find city",
+                    message: "The city could not be found",
                     succes: false,
                 })
             }
         } catch (error) {
             console.log(error)
             res.status("400").json({
-                message: "error",
+                message: "Error",
                 succes: false,
             })
         }
     },
     getCities: async (req, res)=>{
-        const query = req.query
+        //let query = {}
+        let cities
+        let {city} = req.query
+        let regExp= new RegExp(`^${city}`)
+        console.log(regExp)
         try {
-            let cities = await City.find(query? query:null)
-            if (cities) {
+            let cities= await City.find({city: regExp}).exec();
+            if (res.query == true) {
                 res.status("200").json({
-                    message: "you get cities",
+                    message: "The following cities were found",
                     response: cities,
                     succes: true,
                 })
             } else {
                 res.status("404").json({
-                    message: "could't find cities",
+                    message: "No cities could be found...",
                     succes: false,
                 })
             }
         } catch (error) {
             console.log(error)
         }
-    },
+
+},
     modifyCity: async (req, res)=>{
         const { id } = req.params
         let putCity = {}
@@ -68,20 +76,20 @@ const cityController = {
             putCity = await City.findOneAndUpdate({_id:id},req.body,{new:true})
             if (putCity) {
                 res.status("200").json({
-                    message: "you update one city",
+                    message: "You have updated acity",
                     response: putCity,
                     succes: true,
                 })
             } else {
                 res.status("404").json({
-                    message: "could't find city",
+                    message: "Could not find the city",
                     succes: false,
                 })
             }
         } catch (error) {
             console.log(error)
             res.status("400").json({
-                message: "error",
+                message: "Error",
                 succes: false,
             })
         }
@@ -91,13 +99,13 @@ const cityController = {
         try {
             await City.findOneAndDelete({ _id: id })
             res.status("200").json({
-                message: "you delete one city",
+                message: "You deleted a city",
                 succes: true,
             })
         } catch (error) {
             console.log(error)
             res.status("400").json({
-                message: "error",
+                message: "Error",
                 succes: false,
             })
         }

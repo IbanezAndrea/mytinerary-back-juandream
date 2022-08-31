cb529f5588f54729b15572e44a96afcb2b266706
IbanezAndrea <mrs3lite@gmail.com>
2022-08-31 18:06:46 -0300

    getCities fixed

--- controllers/cityController.js
+++ controllers/cityController.js
@@ -8,14 +8,14 @@ const cityController = {
         try {
             let city = await new City(req.body).save()
             res.status("201").json({
-                message: "A new city has been added",
+                message: "A new city has been added.",
                 response: city._id,
                 succes: true,
             })
         } catch (error) {
             console.log(error)
             res.status("400").json({
-                message: "Your city could not be added",
+                message: "Your city could not be added.",
                 succes: false,
             })
         }
@@ -26,13 +26,13 @@ const cityController = {
             let city = await City.findOne({ _id: id })
             if (city) {
                 res.status("200").json({
-                    message: "Found a city",
+                    message: "Found a city.",
                     response: city,
                     succes: true,
                 })
             } else {
                 res.status("404").json({
-                    message: "The city could not be found",
+                    message: "The city could not be found.",
                     succes: false,
                 })
             }
@@ -44,31 +44,33 @@ const cityController = {
             })
         }
     },
-    getCities: async (req, res)=>{
-        //let query = {}
-        let cities
-        let {city} = req.query
-        let regExp= new RegExp(`^${city}`)
-        console.log(regExp)
-        try {
-            let cities= await City.find({city: regExp}).exec();
-            if (res.query == true) {
-                res.status("200").json({
-                    message: "The following cities were found",
-                    response: cities,
-                    succes: true,
-                })
-            } else {
-                res.status("404").json({
-                    message: "No cities could be found...",
-                    succes: false,
-                })
-            }
-        } catch (error) {
-            console.log(error)
-        }
 
+getCities: async (req, res)=>{
+    const query = req.query
+    let cities
+            if(query.city){
+                    let regExp= new RegExp(`^${query.city}`)
+                    query.city= regExp
+                }
+    try {
+        cities = await City.find(query? query:null)
+        if (cities) {
+            res.status("200").json({
+                message: "The following cities were found.",
+                response: cities,
+                succes: true,
+            })
+        } else {
+            res.status("404").json({
+                message: "No cities could be found...",
+                succes: false,
+            })
+        }
+    } catch (error) {
+        console.log(error)
+    }
 },
+
     modifyCity: async (req, res)=>{
         const { id } = req.params
         let putCity = {}
@@ -76,13 +78,13 @@ const cityController = {
             putCity = await City.findOneAndUpdate({_id:id},req.body,{new:true})
             if (putCity) {
                 res.status("200").json({
-                    message: "You have updated acity",
+                    message: "You have updated acity.",
                     response: putCity,
                     succes: true,
                 })
             } else {
                 res.status("404").json({
-                    message: "Could not find the city",
+                    message: "Could not find the city.",
                     succes: false,
                 })
             }
@@ -99,7 +101,7 @@ const cityController = {
         try {
             await City.findOneAndDelete({ _id: id })
             res.status("200").json({
-                message: "You deleted a city",
+                message: "You deleted a city.",
                 succes: true,
             })
         } catch (error) {

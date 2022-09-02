e3b45df0967c4988dd05d479c6368502dfe68724
IbanezAndrea <mrs3lite@gmail.com>
2022-09-02 17:36:56 -0300

    issue-31

--- models/User.js
+++ models/User.js
@@ -4,13 +4,17 @@ const mongoose= require ('mongoose')
         {
         name:{type: String, required:true},
         lastName:{type: String, required:true},
-        mail:{type: String, required:true},
-        password:{type: String, required:true},
+        mail:{ 
+		type : String, min: [4, 'Too short, min is 4 characters'], max: [32, 'Too long, max is 32 characters'],
+		unique : true, lowercase : true, required :true, match : [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/]
+        },
+        password:{
+            type: String, min: [4, 'Too short, min is 4 characters'], max: [32, 'Too long, max is 32 characters'],  required:true
+        },
         photo:{type: String},
         country:{type: String, required:true}
         }
     );
 
     const User= mongoose.model("users", userSchema);
-
 module.exports = User
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var router = express.Router();
-const citiesRouter= require('./cities')
-
+const citiesRouter= require('./cities');
+const userRoutes = require('./users');
+const itinerariesRoutes = require('./itineraries')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render( 'index', {title: 'Tu vieja'});
 });
 
-router.use('/cities',citiesRouter)
+router.use('/cities',citiesRouter);
+router.use('/auth',userRoutes);
+router.use("/itineraries", itinerariesRoutes)
 
 module.exports = router;

5bce7ce8919ab880f3e69f346af22a944b874828
juanPedroGalarza <juanpg232@gmail.com>
2022-09-02 16:10:12 -0300

    get itineraries

--- controllers/itineraryController.js
+++ controllers/itineraryController.js
@@ -3,20 +3,44 @@ const Itinerary = require("../models/Itinerary")
 const itineraryController = {
     addItinerary: async (req, res) => {
         try {
-            const itinerary = new Itinerary(req.body).save()
-            res.status(201).json({
+            let itinerary = await new Itinerary(req.body).save()
+            res.status("201").json({
                 message: "A new itinerary has been added.",
                 response: itinerary._id,
                 succes: true,
             })
         } catch (error) {
             console.log(error)
-            res.status(400).json({
+            res.status("400").json({
                 message: "Your itinerary could not be added.",
                 succes: false,
             })
         }
-    }
+    },
+    getItineraries: async (req, res) => {
+        let itineraries
+        try {
+            itineraries = await Itinerary.find()
+            if (itineraries) {
+                res.status("200").json({
+                    message: "The following itineraries were found.",
+                    response: itineraries,
+                    succes: true,
+            })
+            } else {
+                res.status("404").json({
+                    message: "No itineraries could be found...",
+                    succes: false,
+                })
+            }
+        } catch (error) {
+            console.log(error)
+            res.status("400").json({
+                message: "Your itinerary could not be added.",
+                succes: false,
+            })
+        }
+    } 
 }
 
 module.exports = itineraryController
--- routes/itineraries.js
+++ routes/itineraries.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router= express.Router();
 
-const { addItinerary } = require("../controllers/itineraryController")
+const { addItinerary,getItineraries } = require("../controllers/itineraryController")
 
 router.post('/', addItinerary)
+router.get('/', getItineraries)
 
 module.exports = router

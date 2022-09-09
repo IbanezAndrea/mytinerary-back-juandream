ad1e262e39e16e186d174355e076bda280ecad1c
IbanezAndrea <mrs3lite@gmail.com>
2022-09-09 15:26:06 -0300

    User update

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,22 +1,68 @@
 const User = require('../models/User');
+const crypto = require('crypto')
+const bcryptjs = require('bcryptjs')
 
 const userController ={
-    createUser: async (req, res) => {
-        try {
-            let user = await new User(req.body).save()
-            res.status("201").json({
-                message: "User created ✔",
-                response: user._id,
-                succes: true,
-            })
-        } catch (error) {
-            console.log(error)
-            res.status("400").json({
-                message: "Something went wrong ❌",
-                succes: false,
-            })
-        }
-    },
+    userSingUp: async (req, res) => {
+            const {
+                name,
+                photo,
+                email,
+                passsword,
+                role,
+                from
+            }= req.body
+        try{
+            let user = await User.findOne({email})
+                if (!user){
+                    let logged = false;
+                    let verified = false;
+                    let code =  crypto
+                    .randomBytes(15)
+                    .toString('hex')
+                            if(from === 'form'){ //from form
+                                passsword = bcryptjs.hashSync(pass,10);
+                                user = await new User({name,photo,email,password:[passsword],role,from:[from],logged,verified,code}).save()
+                                //verification email here
+                                    res.status(201).json({
+                                        message: "User signed ✔",
+                                        success: true,
+                                            })
+                                } else{ // from socialmedia
+                                    pass = bcryptjs.hashSync(pass,10);
+                                    verified = true,
+                                    user = await new User({name,photo,email,password:[passsword],role,from:[from],logged}).save()
+                                    res.status(201).json(
+                                            {
+                                                message: "User signed from "+from,
+                                                success: true,
+                                            })
+                                    } 
+                                } else{ //If user exists
+                                    if(user.from.includes(from)){ //if user from property includes from["google"] like so
+                                        res.status(200).json({
+                                            message: "User already exists...",
+                                            success: false 
+                                        })
+                    } else{  // ===> user.from = ['google','facebook'] includes from other socialmedia
+                            user.from.push(from);
+                            user.verified = true;
+                            user.password.push(bcryptjs.hashSync(pass,10))
+                                await user.save()
+                                    res.status(201).json({
+                                        message: "User signed up from "+from,
+                                        success: true
+                                        })
+                                    }}
+                    }catch (error){
+                        console.log(error)
+                                                
+                        res.status(400).json({
+                        message: "could't signed up",
+                        success: false
+                            })
+                        }},
+
     getUser: async (req, res) => {
         const { id } = req.params
         
@@ -27,19 +73,19 @@ const userController ={
                 res.status("200").json({
                     message: "Found ✔",
                     response: user,
-                    succes: true,
+                    success: true,
                 })
             } else {
                 res.status("404").json({
                     message: "Coud not be found ❌",
-                    succes: false,
+                    success: false,
                 })
             }
         } catch (error) {
             console.log(error)
             res.status("400").json({
                 message: "Error",
-                succes: false,
+                success: false,
             })
         }
     },
@@ -55,19 +101,19 @@ const userController ={
                 res.status("200").json({
                     message: "Users found!",
                     response: users,
-                    succes: true,
+                    success: true,
             })
             } else {
                 res.status("404").json({
                     message: "No users could be found...",
-                    succes: false,
+                    success: false,
                 })
             }
         } catch (error) {
             console.log(error)
             res.status("400").json({
                 message: "error",
-                succes: false,
+                success: false,
             })
         }
     },
@@ -80,19 +126,19 @@ const userController ={
                 res.status("200").json({
                     message: "User updated.",
                     response: putUser,
-                    succes: true,
+                    success: true,
                 })
             } else {
                 res.status("404").json({
                     message: "this User does not exist.",
-                    succes: false,
+                    success: false,
                 })
             }
         } catch (error) {
             console.log(error)
             res.status("400").json({
                 message: "Error",
-                succes: false,
+                success: false,
             })
         }
     },
@@ -102,13 +148,13 @@ const userController ={
             await User.findOneAndDelete({ _id: id })
             res.status("200").json({
                 message: "You deleted an User.",
-                succes: true,
+                success: true,
             })
         } catch (error) {
             console.log(error)
             res.status("400").json({
                 message: "Error",
-                succes: false,
+                success: false,
             })
         }
     }

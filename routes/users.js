f358f4daa26656f8ae6446b41a56c4a2a09a7f01
juanPedroGalarza <juanpg232@gmail.com>
2022-09-10 12:12:13 -0300

    route auth/signup

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { getUser, userSignUp,getUsers,modifyUser,removeUser } = require('../controllers/userController' )
 
 
-router.post('/', userSignUp);
+router.post('/signup', userSignUp);
 router.get('/', getUsers );
 router.get('/:id', getUser );
 router.put('/:id', modifyUser );

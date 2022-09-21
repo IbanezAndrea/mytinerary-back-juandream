d7360559ba86397f758b2936ec830f50cab248ce
Ibañez Andrea <108688594+IbanezAndrea@users.noreply.github.com>
2022-09-21 13:08:18 -0300

    city routes/controllers

--- routes/cities.js
+++ routes/cities.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router= express.Router();
+const passport = require('../config/passport');
 
 const {addCity,getOneCity,getCities,modifyCity,removeCity} = require ('../controllers/cityController');
 
-router.post('/',addCity)
+router.post('/',passport.authenticate('jwt',{ session: false}),addCity)
 router.get('/:id',getOneCity)
 router.get('/',getCities)
-router.put('/:id',modifyCity)
+router.put('/:id',passport.authenticate('jwt',{ session: false}),modifyCity)
 router.delete('/:id',removeCity)
 
 module.exports = router;
